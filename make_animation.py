976bcf3da2d7283587cd9f315904b8afd3ae5d6a
Fl-ppie <sebastiaan.aloisius@gmail.com>
2024-11-25 22:25:39 +0100

    Update make_animation.py

--- make_animation.py
+++ make_animation.py
@@ -13,7 +13,6 @@ def compare(x, y):
     if int(y)>int(x):
         return y
         
-# this is just a test
 
 def make_gif():
     frames = [Image.open(image) for image in sorted(glob.glob("*.png"), key=compare)]
