2655ca7189690d6de9e7c8d9b2d21fc389d722b4
Fl-ppie <sebastiaan.aloisius@gmail.com>
2024-12-02 20:22:35 +0100

    renamed newton images

--- z0.0086/position_data/rename_files.py
+++ z0.0086/position_data/rename_files.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-directory = 'images_mond_test/'
+directory = 'images_newtonian_renamed/'
 
 for path in glob.glob(directory+'*.png'):
     os.rename(path, path.replace(r'\0',r'\\').replace(r'\0',r'\\'))
